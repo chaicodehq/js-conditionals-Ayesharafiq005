920899d4c80b5e1c48062f029a2bfba69312f019
Ayesharafiq005 <iamayesharafiq@gmail.com>
2026-02-10 21:12:26 +0500

    7th task completed

--- src/07-coffee-shop.js
+++ src/07-coffee-shop.js
@@ -31,5 +31,44 @@
  * @returns {number} Total price or -1 for invalid input
  */
 export function calculateCoffeePrice(size, type, extras = {}) {
-  // Your code here
+ let price ;
+  if(size != "small" && size != "medium" && size != "large") return -1 ;
+  if(type != "regular" && type != "latte" && type != "cappuccino" && type != "mocha") return -1;
+
+  if(size == "small"){
+    price = 3.00;
+  }
+   else if(size == "medium"){
+    price = 4.00;
+  }
+   else{
+    price = 5.00;
+  }
+
+  if(type == "regular"){
+    price = price ;
+  }
+   else if(type == "latte"){
+    price = price + 1.00 ;
+  }
+   else if(type == "cappuccino"){
+    price = price + 1.50 ;
+  }
+   else{
+    price = price + 2.00 ;
+  }
+
+
+  if(extras.whippedCream){
+    price =  price + 0.50;
+    // return price;
+  }
+  if(extras.extraShot){
+    price = price + 0.75;
+    // return price;
+}
+
+price.toFixed(2);
+ return price;
+
 }
