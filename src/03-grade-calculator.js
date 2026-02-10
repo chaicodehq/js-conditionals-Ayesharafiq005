7751497b1a6fc9bbb8976f2ac28c9d9db5408827
Ayesharafiq005 <iamayesharafiq@gmail.com>
2026-02-10 16:36:42 +0500

    completed task 3

--- src/03-grade-calculator.js
+++ src/03-grade-calculator.js
@@ -25,5 +25,42 @@
  * @returns {string} The letter grade or "INVALID"
  */
 export function calculateGrade(score, hasExtraCredit) {
-  // Your code here
+
+  if(score < 0 || score >100){
+    return "INVALID"; 
+  }
+
+  if(hasExtraCredit){
+    if(score >= 90 && score <=100){
+      score = 5 + score;
+      if(score > 100){
+        score = 100
+        // return "A";
+      }
+    }
+if(score >=90 && score<=100){
+  return "A";
+}
+
+    if(score >= 80 && score <=89){
+      score = 5 + score;
+     return "B";
+    }
+
+    if(score >= 70 && score <=79){
+      score = 5 + score;
+     return "C";
+    }
+
+    if(score >= 60 && score <=69){
+      score = 5 + score;
+      return "D";
+    }
+
+    if(score >= 0 && score <59){
+      score = 5 + score;
+     return "F"; 
+    }
+  }
+
 }
